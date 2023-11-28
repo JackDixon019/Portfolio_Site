b45f8ce84b99d1e9a87212ffd28b10f6af3f321b
JackDixon019 <13709@coderacademy.edu.au>
2023-11-28 11:09:54 +1100

    feat: fixed up some stylings on contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -5,39 +5,40 @@ import EnvelopeIcon from "@heroicons/react/24/solid/EnvelopeIcon";
 import DocumentIcon from "@heroicons/react/24/solid/DocumentTextIcon";
 
 export default function ContactPage() {
-    let contactLinkStyle = "mt-4 flex flex-row justify-center align-middle text-scarlet-500 hover:text-smalt-700"
+    let contactDivStyle = "mt-4 flex flex-row justify-center align-middle text-scarlet-500 hover:text-smalt-700"
+    let contactLinkStyle = "font-Bebas-Neue text-3xl font-bold ml-4 mt-auto"
     return (
         <JaggedSection>
             <h1 className={titleOnPapayaStyle}>Contact</h1>
             <div className="m-auto mt-12 w-6/12 text-center font-Montserrat ">
                 <a href="https://github.com/JackDixon019">
-                    <div className={contactLinkStyle}>
+                    <div className={contactDivStyle}>
                         <span class={"fa-brands fa-github text-3xl "} />
-                        <h2 className={linkBaseStyle + " ml-4 mt-auto"}>
+                        <h2 className={contactLinkStyle}>
                             Github
                         </h2>
                     </div>
                 </a>
                 <a href="http://www.linkedin.com/in/jack-dixon-dev">
-                    <div className={contactLinkStyle}>
+                    <div className={contactDivStyle}>
                         <span class={"fa-brands fa-linkedin text-3xl "} />
-                        <h2 className={linkBaseStyle + " ml-4 mt-auto"}>
+                        <h2 className={contactLinkStyle}>
                             LinkedIn
                         </h2>
                     </div>
                 </a>
                 <a href="mailto:jack.dixon019@gmail.com">
-                    <div className={contactLinkStyle}>
+                    <div className={contactDivStyle}>
                         <EnvelopeIcon className="h-8" />
-                        <h2 className={linkBaseStyle + " ml-4 mt-auto"}>
+                        <h2 className={contactLinkStyle}>
                             Email
                         </h2>
                     </div>
                 </a>
                 <a href={resume}>
-                    <div className={contactLinkStyle}>
+                    <div className={contactDivStyle}>
                         <DocumentIcon className="h-8" />
-                        <h2 className={linkBaseStyle + " ml-4 mt-auto"}>
+                        <h2 className={contactLinkStyle}>
                             Resume
                         </h2>
                     </div>
