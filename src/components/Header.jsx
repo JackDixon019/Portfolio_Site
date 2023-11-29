927f465188dda690e6504b85e0d9dbab3e197e69
JackDixon019 <13709@coderacademy.edu.au>
2023-11-29 15:24:10 +1100

    feat: header text colours now also update according to section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,67 @@
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
+export default function Header() {
+    let [headerColour, setHeaderColour] = useState("bg-papaya-whip-100");
+    let [navbarTextColour, setNavbarTextColour] = useState(
+        " text-scarlet-500 hover:text-smalt-700",
+    );
 
-export default function Header(){
+    let colourMatch = {
+        "bg-papaya-whip-100":
+            " text-scarlet-500 hover:text-smalt-700",
+        "bg-smalt-800": " text-orange-peel-400 hover:text-scarlet-500",
+        "bg-orange-peel-500": " text-smalt-700 hover:text-papaya-whip-100",
+    };
 
-    let [headerColour, setHeaderColour] = useState("bg-papaya-whip-100")
-
-    function changeColour(){
-        let element = document.elementFromPoint(1,49)
-        let regex = /(bg-*).+/g
-        setHeaderColour(regex.exec(element.className)[0])
-        console.log(headerColour)
+    // Change header colours to match current top section
+    function changeColour() {
+        // Targets 10 pixels below header
+        let target = document.elementFromPoint(
+            1,
+            document.getElementById("header").getBoundingClientRect().bottom +
+                10,
+        );
+        let bgRegex = /(bg-*).+/g
+        setHeaderColour(bgRegex.exec(target.className)[0].split(" ")[0]);
+        setNavbarTextColour(colourMatch[headerColour]);
     }
 
     useEffect(() => {
-        const onScroll = () => changeColour()
-        window.addEventListener("scroll", onScroll, {passive: true});
-        return () => window.removeEventListener("scroll", onScroll)
-    }, [headerColour])
+        const onScroll = () => changeColour();
+        window.addEventListener("scroll", onScroll, { passive: true });
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [headerColour]);
 
-    let navbarStyle = "text-4xl font-Bebas-Neue "
-    return(
-        <header id="header" className={"w-full grid grid-cols-6 z-10 sticky top-0 left-0 right-0 h-12 " + "text-scarlet-500 hover:text-smalt-700 " + headerColour}>
-            <logo id="logo" className="text-4xl w-12 mt-1 col-start-1  ml-3 font-Rubik "> <NavLink to={"/"}>JD</NavLink></logo>
-            <nav className="col-start-3 mt-2 flex-row flex justify-evenly col-span-4 md:col-start-5 md:col-span-2">
-                <NavLink to={"/"} className={navbarStyle}>Home</NavLink>
+    let navbarStyle = "text-4xl font-Bebas-Neue " + navbarTextColour;
+    return (
+        <header
+            id="header"
+            className={
+                "sticky left-0 right-0 top-0 z-10 grid h-12 w-full grid-cols-6 " +
+                headerColour
+            }
+        >
+            <nav
+                id="logo"
+                className={
+                    "col-start-1 ml-3 mt-1 w-12  font-Rubik text-4xl  " +
+                    navbarTextColour
+                }
+            >
+                {" "}
+                <NavLink to={"/"}>JD</NavLink>
+            </nav>
+            <nav className="col-span-4 col-start-3 mt-2 flex flex-row justify-evenly md:col-span-2 md:col-start-5">
+                <NavLink to={"/"} className={navbarStyle}>
+                    Home
+                </NavLink>
                 {/* TODO: make blog page */}
                 {/* <NavLink to={"/blog"} className={navbarStyle}>Blog</NavLink> */}
-                <NavLink to={"/contact"} className={navbarStyle}>Contact</NavLink>
+                <NavLink to={"/contact"} className={navbarStyle}>
+                    Contact
+                </NavLink>
             </nav>
         </header>
-    )
+    );
 }
