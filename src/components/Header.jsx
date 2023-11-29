f393edcf910982c85d1337c91f427c5c747376b0
JackDixon019 <13709@coderacademy.edu.au>
2023-11-29 14:25:37 +1100

    Feat: header background colour now changes to match section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 
 export default function Header(){
-    let navbarStyle = "text-4xl text-scarlet-500 font-Bebas-Neue hover:text-smalt-700"
+
+    let [headerColour, setHeaderColour] = useState("bg-papaya-whip-100")
+
+    function changeColour(){
+        let element = document.elementFromPoint(1,49)
+        let regex = /(bg-*).+/g
+        setHeaderColour(regex.exec(element.className)[0])
+        console.log(headerColour)
+    }
+
+    useEffect(() => {
+        const onScroll = () => changeColour()
+        window.addEventListener("scroll", onScroll, {passive: true});
+        return () => window.removeEventListener("scroll", onScroll)
+    }, [headerColour])
+
+    let navbarStyle = "text-4xl font-Bebas-Neue "
     return(
-        <header id="header" className="w-full grid grid-cols-6 z-10 sticky top-0 left-0 right-0 h-12 bg-papaya-whip-100 jagged">
-            <logo id="logo" className="text-4xl w-12 mt-1 col-start-1 text-scarlet-500 ml-3 font-Rubik hover:text-smalt-700"> <NavLink to={"/"}>JD</NavLink></logo>
+        <header id="header" className={"w-full grid grid-cols-6 z-10 sticky top-0 left-0 right-0 h-12 " + "text-scarlet-500 hover:text-smalt-700 " + headerColour}>
+            <logo id="logo" className="text-4xl w-12 mt-1 col-start-1  ml-3 font-Rubik "> <NavLink to={"/"}>JD</NavLink></logo>
             <nav className="col-start-3 mt-2 flex-row flex justify-evenly col-span-4 md:col-start-5 md:col-span-2">
                 <NavLink to={"/"} className={navbarStyle}>Home</NavLink>
                 {/* TODO: make blog page */}
